f711d7bb579ab8b32b2b65d35fbefcb629246d64
Michael Semko <msemko@gmail.com>
2025-11-16 08:51:51 +0000

    enable eslint on build, enable @ alias for src

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,30 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import eslint from "vite-plugin-eslint";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    eslint({
+      cache: false,          // ensures fresh results every run
+      include: ["src/**/*.{ts,tsx,jsx}"], // adjust path as needed
+      failOnWarning: false,
+      failOnError: false,     // set true if you want HMR to stop on errors
+      emitError: true,
+    })
+  ], 
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.mjs",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "../"),
+      },
+    },
   },
 });
+
